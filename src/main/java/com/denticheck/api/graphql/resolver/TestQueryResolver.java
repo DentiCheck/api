c7e44fd5b19acbea99d4badbe6a5c4af09818bbc
Chu-HoYeon <chu.hoyeon.dev@gmail.com>
2026-02-07 03:38:07 +0900

    fix(cors): deactivate duplicated CorsConfig; React GraphQL request passes

---
+++ src/main/java/com/denticheck/api/graphql/resolver/TestQueryResolver.java
@@ -0,0 +1,30 @@
+package com.denticheck.api.graphql.resolver;
+
+import com.denticheck.api.domain.user.entity.UserEntity;
+import com.denticheck.api.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class TestQueryResolver {
+
+    private final UserRepository userRepository;
+
+    // ✅ 로그인한 내 정보 조회
+    @QueryMapping
+    @PreAuthorize("hasRole('USER')")
+    public UserEntity me(Authentication authentication) {
+        String username = authentication.getName(); // JWTFilter가 setAuthentication 해줘야 함
+
+        UserEntity entity = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("user not found"));
+
+        return entity;
+    }
+
+}
